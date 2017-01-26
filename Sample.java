0dd3b190b2a5ee15a356d67925797d1c9614c307
Vinoth <vino.vino1991@gmail.com>
2017-01-26 19:12:41 +0530

    First commit

---
+++ Sample.java
@@ -0,0 +1,6 @@
+public class Sample {
+public static void main(String []v)
+{
+System.out.println("venkat771");
+}
+}
---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test
+{
+public static void main (String []jinken)
+{
+System.out.println("Welcome to Java");
+}
+}
